5264ab27eeec0c23978e243a92e4e97dbfb1c291
kim <rkdtks3783@naver.com>
2022-01-11 00:22:08 +0900

    file upload

--- multicampus/python/lecture_multi/my_modules.py
+++ multicampus/python/lecture_multi/my_modules.py
@@ -1,3 +1,12 @@
-import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 15:17:48 2022
+
+@author: rkdtk
+"""
+
 import pandas as pd
 from pandas import Series, DataFrame
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
